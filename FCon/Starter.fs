63a670d7919881c10f27a6637c067e116a7a3ac1
Zollern Wolf <admin@zollernverse.org>
2018-12-15 18:39:05 -0500

    Starter program

---
+++ FCon/Starter.fs
@@ -0,0 +1,12 @@
+﻿open System
+
+let test num =
+    if num < 5 then "below five"
+    elif num > 5 then "above five"
+    else "was not a number"
+ 
+let main() =
+    Console.WriteLine("Number was {0}", (test 10))
+    Console.ReadKey() |> ignore
+
+main()
